31a0a28de98042910f74805b10e83d4945254a0d
Juho Mäkeläinen <juhowm@metropolia.fi>
2023-10-24 10:09:08 +0300

    tehtävä 9.4 siistitty

--- moduuli 9 tehtävät/tehtävä 9.4.py
+++ moduuli 9 tehtävät/tehtävä 9.4.py
@@ -24,28 +24,19 @@ class Auto:
         self.dis += self.cur * hours
 
 
-# autolist = []
-# amount = 0
-# while amount < 10:
-# amount += 1
-# autolist.append(f"ABC-{amount}")
-# print(autolist)
 autot = []
 voitto = False
 for n in range(1, 11):
-    newcar = Auto(f"ABC-{n}", random.randint(100, 200))
-    autot.append(newcar)
+    uusauto = Auto(f"ABC-{n}", random.randint(100, 200))
+    autot.append(uusauto)
 while voitto is False:
     for auto in autot:
-        if auto.dis < 10000:
-            auto.kiihdytä(random.randint(-10, 15))
-            auto.kulje(1)
-            if auto.dis >= 10000:
-                voitto = True
-                print("|{:^20}|{:^20}|{:^20}|{:^20}|".format("rekisteri", "max. nopeus (km/h)", "nykynopeus (km/h)", "etäisyys (km)"))
-                for auto in autot:
-                    print("|{:20}|{:<20}|{:<20}|{:<20}|".format(vars(auto)["rek"], vars(auto)["top"], vars(auto)["cur"], vars(auto)["dis"]))
-
-        else:
+        auto.kiihdytä(random.randint(-10, 15))
+        auto.kulje(1)
+        if auto.dis >= 10000:
             voitto = True
-            print(vars(auto))
+            print("|{:^20}|{:^20}|{:^20}|{:^20}|".format("rekisteri", "max. nopeus (km/h)", "nykynopeus (km/h)"
+                                                         , "etäisyys (km)"))
+            for auto in autot:
+                print("|{:20}|{:<20}|{:<20}|{:<20}|".format(vars(auto)["rek"], vars(auto)["top"],
+                                                            vars(auto)["cur"], vars(auto)["dis"]))
