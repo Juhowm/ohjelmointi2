11cf21a71d85318ca1f6a0bf7a3a16a92f7a1997
Juho Mäkeläinen <juhowm@metropolia.fi>
2023-11-06 15:32:05 +0200

    roska pois

--- moduuli 12 tehtävät/12.2.py
+++ moduuli 12 tehtävät/12.2.py
@@ -1,8 +1,4 @@
 import requests
-import json
-
-#https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={372576880cc1ce2ce575eea01430304e}
-#sijaintimuunnos
 
 kp = input("Anna paikkakunnan nimi: ")
 sää = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={kp}&appid=372576880cc1ce2ce575eea01430304e&lang=fi&units=metric").json()
